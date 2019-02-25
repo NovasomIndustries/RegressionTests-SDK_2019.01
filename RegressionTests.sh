d19698d3b4add85679acdb66867f33552623787d
NOVAsomIndustries <filippo.visocchi@novasomindustries.com>
2019-02-25 14:38:00 +0100

    Scripts cleanup

--- RegressionTests.sh
+++ RegressionTests.sh
@@ -1,6 +1,62 @@
-#!/bin/sh
+#!/bin/bash
+if [ -z $1 ]; then
+	echo "Choose board : P , U or M7"
+	exit
+fi
 
-./M7regression_tests.sh
-./Pregression_tests.sh
-./Uregression_tests.sh
+[ "$1" == "P" ] && . ./Pboard.inc
+[ "$1" == "U" ] && . ./Uboard.inc
+[ "$1" == "M7" ] && . ./M7board.inc
+
+HERE=`pwd`
+
+NOW=`date +%Y-%d-%m_%H-%M`
+
+cd ${SDK}/Utils
+rm -rf ${SDK}/${FILESYSTEM_DIR}/${REGTESTFS}
+echo "Regression tests started at ${NOW}" > ${REGTESTLOG}-${NOW}
+echo "Testing ${BOARD_NAME} board at `date +%Y-%d-%m.%H:%M`" >> ${REGTESTLOG}-${NOW}
+./${SETUPFS_CMD}
+if ! [ "$?" == "0" ]; then
+	echo "	Filesystem Generation : FAIL" >> ${REGTESTLOG}-${NOW}
+else
+	echo "	Filesystem Generation : Pass" >> ${REGTESTLOG}-${NOW}
+fi
+cd ${SDK}/${FILESYSTEM_DIR}/${REGTESTFS}
+make
+if ! [ "$?" == "0" ]; then
+	echo "	Filesystem Compilation : FAIL" >> ${REGTESTLOG}-${NOW}
+else
+	echo "	Filesystem Compilation : Pass" >> ${REGTESTLOG}-${NOW}
+fi
+cd ${BOARD_DIR}
+${KMAKE_CMD}
+if ! [ "$?" == "0" ]; then
+	echo "	Kernel : FAIL" >> ${REGTESTLOG}-${NOW}
+else
+	echo "	Kernel : Pass" >> ${REGTESTLOG}-${NOW}
+fi
+cd ${HERE}
+cp ${BSPF_SAMPLE}.bspf ${SDK}/${DtbUserWorkArea_DIR}/.
+cd ${SDK}/${UTILS_DIR}
+${PARSER_CMD}
+${COMPILER_CMD}
+if ! [ "$?" == "0" ]; then
+	echo "	Dtb Parser : FAIL" >> ${REGTESTLOG}-${NOW}
+else
+	echo "	Dtb Parser : Pass" >> ${REGTESTLOG}-${NOW}
+fi
+if ! [ "$?" == "0" ]; then
+	echo "	DTB Compiler : FAIL" >> ${REGTESTLOG}-${NOW}
+else
+	echo "	DTB Compiler : Pass" >> ${REGTESTLOG}-${NOW}
+fi
+cd ${BOARD_DIR}
+${MAKE_UBOOT_CMD}
+if ! [ "$?" == "0" ]; then
+        echo "	u-boot compile : FAIL" >> ${REGTESTLOG}-${NOW}
+else
+        echo "	u-boot compile : Pass" >> ${REGTESTLOG}-${NOW}
+fi
+echo "Regression tests finished at `date +%Y-%d-%m.%H:%M`" >> ${REGTESTLOG}-${NOW}
 
