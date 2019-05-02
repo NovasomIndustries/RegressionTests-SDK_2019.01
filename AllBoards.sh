cf06c00905ecc384bbfa134c8d6efb9696d42ed6
novasomindustries <novasomindustries@Deb9.Deb9>
2019-05-02 14:16:37 +0200

    Fixed path for new sdk location

--- AllBoards.sh
+++ AllBoards.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-CFGS=`ls /Devel/NOVAsdk2019.01/Utils/Configurations/PClass_Buildroot_*`
+# just include one of them as we use only $SDK variable ...
+. ./inc/M7board.inc
+CFGS=`ls ${SDK}/Utils/Configurations/PClass_Buildroot_*`
 for i in ${CFGS}; do
 	FILE=`basename ${i}`
 	./RegressionTests.sh P ${FILE}
 done
-CFGS=`ls /Devel/NOVAsdk2019.01/Utils/Configurations/U5Class_Buildroot_*`
+CFGS=`ls ${SDK}/Utils/Configurations/U5Class_Buildroot_*`
 for i in ${CFGS}; do
 	FILE=`basename ${i}`
 	./RegressionTests.sh U ${FILE}
 done
-CFGS=`ls /Devel/NOVAsdk2019.01/Utils/Configurations/M7Class_Buildroot_*`
+CFGS=`ls ${SDK}/Utils/Configurations/M7Class_Buildroot_*`
 for i in ${CFGS}; do
 	FILE=`basename ${i}`
 	./RegressionTests.sh M7 ${FILE}
